98c2c902d14d490327412829ed7a29e784fcdcc0
Krystalz42 <aroulin@student.42.fr>
2018-10-07 19:05:32 +0200

    nuiblanch

---
+++ .deps/Ship/ShipBigBoss.d
@@ -0,0 +1,35 @@
+.objs/Ship/ShipBigBoss.o: srcs/Ship/ShipBigBoss.cpp \
+  incs/Ship/ShipBigBoss.hpp incs/Ship/AShip.hpp incs/utils/IShooter.hpp \
+  incs/Bullets/ABullet.hpp incs/ft_retro.h incs/utils/Position.hpp \
+  incs/utils/List.hpp incs/utils/IBulletsManager.hpp \
+  incs/utils/ICollision.hpp incs/utils/IShipsManager.hpp \
+  incs/Weapon/AWeapon.hpp incs/Weapon/WeaponTripleDiagonal.hpp \
+  incs/Weapon/WeaponTripleVertical.hpp incs/Weapon/WeaponUltime.hpp
+
+incs/Ship/ShipBigBoss.hpp:
+
+incs/Ship/AShip.hpp:
+
+incs/utils/IShooter.hpp:
+
+incs/Bullets/ABullet.hpp:
+
+incs/ft_retro.h:
+
+incs/utils/Position.hpp:
+
+incs/utils/List.hpp:
+
+incs/utils/IBulletsManager.hpp:
+
+incs/utils/ICollision.hpp:
+
+incs/utils/IShipsManager.hpp:
+
+incs/Weapon/AWeapon.hpp:
+
+incs/Weapon/WeaponTripleDiagonal.hpp:
+
+incs/Weapon/WeaponTripleVertical.hpp:
+
+incs/Weapon/WeaponUltime.hpp:
---
+++ .deps/Weapon/AWeapon.d
@@ -0,0 +1,13 @@
+.objs/Weapon/AWeapon.o: srcs/Weapon/AWeapon.cpp incs/utils/Position.hpp \
+  incs/Weapon/AWeapon.hpp incs/Bullets/ABullet.hpp incs/ft_retro.h \
+  incs/utils/List.hpp
+
+incs/utils/Position.hpp:
+
+incs/Weapon/AWeapon.hpp:
+
+incs/Bullets/ABullet.hpp:
+
+incs/ft_retro.h:
+
+incs/utils/List.hpp:
